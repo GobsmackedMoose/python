2e82b75265d4a75914a6016cd24bc211250c7cf4
Sarah Peeralam <speeralam@gmail.com>
2026-04-07 16:06:59 -0500

    wrote television file

---
+++ television.py
@@ -0,0 +1,53 @@
+
+
+class Television():
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0 
+    MAX_CHANNEL = 3
+    
+    def __init__(self):
+        self.status = False
+        self.muted = False
+        self.volume = self.MIN_VOLUME
+        self.channel = self.MIN_CHANNEL
+
+    def power(self):
+        self.status = not(self.status)
+
+    def mute(self):
+        if self.status:
+            self.muted = not(self.muted)
+
+    def channel_up(self):
+        if self.status:
+            self.channel += 1
+            if self.channel > self.MAX_CHANNEL:
+                self.channel = self.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.status:
+            self.channel -= 1
+            if self.channel < self.MIN_CHANNEL:
+                self.channel = self.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.muted:
+            self.muted = False
+        if self.status:
+            if self.volume < self.MAX_VOLUME:
+                self.volume += 1
+
+    def volume_down(self):
+        if self.muted:
+            self.muted = False
+        if self.status:
+            if self.volume > self.MIN_VOLUME:
+                self.volume -= 1
+
+    def __str__(self):
+        if not(self.muted):
+            return f'Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}'
+        else:
+            return f'Power = {self.status}, Channel = {self.channel}, Volume = 0'
+
